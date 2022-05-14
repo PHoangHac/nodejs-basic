ed971a1d6deccf0fe354645d4de254cfecb9e6b3
hoanghac <khongcono92@example.com>
2022-05-14 15:44:46 +0700

    static nodejs

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require ('express-handlebars');
+const path = require('path');
+//import thu vien
+
+
+const app = express()
+//tao mot bien app gan ==
+//goi lai ham express --> duoc tao san trong thu vien
+
+const port = 7070
+//khoi tao cong localhost
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('combined'))
+//hien thi log de biet resquest len server ntn ?
+
+//template engine
+//app dang su dung engine la handlebars
+app.engine('hbs', engine(
+  {
+    extname: '.hbs'
+  }
+));
+//set ungdung express su dung view engine la handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
